a517f9ec8ff5d101a235ffe2b91d6eae76cf437c
Amit Gupta <amitgupta758000@gmail.com>
2024-05-17 19:04:03 +0530

    Changed DEBUG to deploymeny

--- card_games/settings.py
+++ card_games/settings.py
@@ -26,7 +26,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR,'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True # os.environ.get("DEBUG", "False").lower() == "true"
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
 ALLOWED_HOSTS = []
 
